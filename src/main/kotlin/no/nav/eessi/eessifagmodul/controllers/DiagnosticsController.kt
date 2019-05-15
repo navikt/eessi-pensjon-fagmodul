272e1b73f444e0cf8f485fc72e0210f11791f4a2
Jens Madsen <jens.christian.madsen@nav.no>
2019-05-15 15:33:58 +0200

    P7000 prefill med lett data. la tilbake ping.

--- src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/DiagnosticsController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/DiagnosticsController.kt
@@ -1,51 +1,30 @@
 package no.nav.eessi.eessifagmodul.controllers
 
-//@CrossOrigin
-//@RestController
-//class DiagnosticsController {
-//
-//    @Value("\${app.name}")
-//    lateinit var appName: String
-//
-//    @Value("\${app.version}")
-//    private lateinit var appVersion: String
-//
-//    private val logger: Logger by lazy { LoggerFactory.getLogger(DiagnosticsController::class.java) }
-//
-//    @GetMapping("/ping")
-//    fun ping(): ResponseEntity<Unit> {
-//        return ResponseEntity.ok().build()
-//    }
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-//    @GetMapping("/internal/selftest")
-//    fun selftest(): SelftestResult {
-//        logger.debug("Selftest passed")
-//        return SelftestResult(name = appName, version = appVersion, aggregateResult = 0, checks = null)
-//    }
+@CrossOrigin
+@RestController
+class DiagnosticsController {
 
-//    @GetMapping("/internal/isalive")
-//    fun isalive(): ResponseEntity<String> {
-//        return ResponseEntity.ok("Is alive")
-//    }
-//
-//    @GetMapping("/internal/isready")
-//    fun isready(): ResponseEntity<String> {
-//        return ResponseEntity.ok("Is ready")
-//    }
-//
-//}
+    private val logger: Logger by lazy { LoggerFactory.getLogger(DiagnosticsController::class.java) }
 
-//data class SelftestResult(
-//        val name: String,
-//        val version: String,
-//        val timestamp: Instant = Instant.now(),
-//        val aggregateResult: Int,
-//        val checks: List<Check>?
-//)
-//
-//data class Check(
-//        val endpoint: String,
-//        val description: String,
-//        val errorMessage: String,
-//        val result: Int
-//)
+    @Value("\${app.name}")
+    lateinit var appName: String
+
+    @Value("\${app.version}")
+    private lateinit var appVersion: String
+
+
+    @GetMapping("/ping")
+    fun ping(): ResponseEntity<Unit> {
+        logger.debug("Ping kalt, så alt er ok")
+        return ResponseEntity.ok().build()
+    }
+
+}
