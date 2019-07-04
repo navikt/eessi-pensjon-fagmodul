5830d1f087f5fa9fbdbe2b65dde2919756062d71
Mo Amini <mo.amini@nav.no>
2019-07-04 14:12:28 +0200

    Mappet P14000

--- src/test/kotlin/no/nav/eessi/eessifagmodul/models/SedP14000Test.kt
+++ src/test/kotlin/no/nav/eessi/eessifagmodul/models/SedP14000Test.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import org.skyscreamer.jsonassert.JSONAssert
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.test.assertEquals
 
 class SedP14000Test : AbstractSedTest() {
 
@@ -14,6 +15,9 @@ class SedP14000Test : AbstractSedTest() {
 
         val p14000json = getTestJsonFile("P14000-NAV.json")
         val p14000sed = getSEDfromTestfile(p14000json)
+
+        assertEquals("342", p14000sed.nav?.bruker?.endringer?.personpinendringer?.gammelt)
+        assertEquals("341", p14000sed.nav?.bruker?.endringer?.personpinendringer?.nytt)
 
         val json = p14000sed.toJson()
         JSONAssert.assertEquals(p14000json, json, false)
