e13fa844e693060dc7cfedc8bf72c0bfe3c4c7cb
Mo Amini <mo.amini@nav.no>
2019-08-20 12:02:21 +0200

    PersonV3 mock test
    Legget scafolding til PersonV3 testing til
    Fjernes timingservice for PersonV3 da dette er en rask og vidt brukt tjeneste i NAV. Enten er den superrask eller så er den helt nede og hele NAV hopper

--- src/test/kotlin/no/nav/eessi/pensjon/services/personv3/PersonV3ServiceTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/services/personv3/PersonV3ServiceTest.kt
@@ -1,34 +1,22 @@
 package no.nav.eessi.pensjon.services.personv3
 
-import com.nhaarman.mockitokotlin2.doNothing
-import com.nhaarman.mockitokotlin2.whenever
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import io.mockk.mockkStatic
-import no.nav.eessi.pensjon.metrics.TimingService
-import no.nav.eessi.pensjon.security.sts.STSClientConfig
 import no.nav.eessi.pensjon.security.sts.configureRequestSamlToken
+import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonPersonIkkeFunnet
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
+import no.nav.tjeneste.virksomhet.person.v3.feil.PersonIkkeFunnet
 import org.junit.After
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
-
-//@RunWith(MockitoJUnitRunner::class)
 class PersonV3ServiceTest {
 
     @MockK
     private lateinit var personV3Mock: PersonV3
-
-    @MockK
-    lateinit var timingServiceMock: TimingService
 
     @InjectMockKs
     lateinit var personV3Service: PersonV3Service
@@ -36,22 +24,14 @@ class PersonV3ServiceTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        personV3Service = PersonV3Service(personV3Mock, timingServiceMock)
-        mockkStatic(STSClientConfig::class)
-        every {  configureRequestSamlToken(personV3Mock) } returns mockk()
+        mockkStatic("no.nav.eessi.pensjon.security.sts.STSClientConfigKt")
+        every { configureRequestSamlToken(personV3Mock) } returns Unit
     }
 
-    @Test
-    fun hentPersonPing() {
-        assertTrue(personV3Service.hentPersonPing())
-    }
-
-    @Test
+    @Test(expected = PersonV3IkkeFunnetException::class)
     fun hentPerson() {
         val fnr = "18128126178"
-       //whenever(configureRequestSamlToken(personV3Mock)).
-        whenever(personV3Service.hentPerson(fnr)).thenThrow(PersonV3IkkeFunnetException("EXPECTED"))
-
+        every { personV3Mock.hentPerson(any()) } throws HentPersonPersonIkkeFunnet("EXPECTED", PersonIkkeFunnet())
         val response = personV3Service.hentPerson(fnr)
     }
 
