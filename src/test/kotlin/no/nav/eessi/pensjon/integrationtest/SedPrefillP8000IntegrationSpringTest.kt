63c87bd17570fa7c25b864b20a7eb7419cb3572d
Daniel Skarpås <dskarpas@gmail.com>
2021-01-10 15:51:43 +0100

    EP-1060: henter ut kravdato for prefilling  - hentKravDato, henter ut der det ikke eksisterer vedtak  - hentKravDatoFraVedtak, henter ut direkte fra vedtak

--- src/test/kotlin/no/nav/eessi/pensjon/integrationtest/SedPrefillP8000IntegrationSpringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/integrationtest/SedPrefillP8000IntegrationSpringTest.kt
@@ -14,6 +14,8 @@ import no.nav.eessi.pensjon.personoppslag.aktoerregister.NorskIdent
 import no.nav.eessi.pensjon.personoppslag.personv3.PersonV3Service
 import no.nav.eessi.pensjon.security.sts.STSService
 import no.nav.eessi.pensjon.services.kodeverk.KodeverkClient
+import no.nav.eessi.pensjon.services.pensjonsinformasjon.EPSaktype
+import no.nav.eessi.pensjon.services.pensjonsinformasjon.KravArsak
 import no.nav.pensjon.v1.kravhistorikk.V1KravHistorikk
 import no.nav.pensjon.v1.kravhistorikkliste.V1KravHistorikkListe
 import no.nav.pensjon.v1.pensjonsinformasjon.Pensjonsinformasjon
