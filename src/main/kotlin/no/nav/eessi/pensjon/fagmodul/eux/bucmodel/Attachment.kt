6e01b9d119ffb5d1f66ccb6c06760237f5f262ee
moamini <mo.amini@nav.no>
2021-03-23 09:28:55 +0100

    Sletter ubrukte BUC klasser og felter og tilhørende tester som ikke brukes i prod koden

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/bucmodel/Attachment.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/bucmodel/Attachment.kt
@@ -1,20 +1,13 @@
 package no.nav.eessi.pensjon.fagmodul.eux.bucmodel
 
 class Attachment(
-        val creator: Creator? = null,
         val fileName: String? = null,
         val medical: Boolean? = null,
         val mimeType: String? = null,
-        val creationDate: Any? = null,
-        val internalId: Any? = null,
-        val versions: List<VersionsItem?>? = null,
-        val hasMultipleVersions: Boolean? = null,
         val lastUpdate: Any? = null,
-        val caseId: String? = null,
         val name: String? = null,
         val documentId: String? = null,
         val id: String? = null,
-        val parentDocumentId: Any? = null
 )
 
 class ShortAttachment(
