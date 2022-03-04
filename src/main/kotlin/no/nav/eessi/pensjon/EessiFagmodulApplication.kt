0b0f03252aa5e0fd7fc110b0365064c00ac13949
Jens Chr. Madsen <42404227+JensNav@users.noreply.github.com>
2022-03-04 09:32:16 +0100

    Feature/ser paa auth exception mot prefill (#428)
    * legger inn konfig endring for prefill-rest template
    
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/EessiFagmodulApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiFagmodulApplication.kt
@@ -8,7 +8,7 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Profile
 
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation", "no.nav.eessi.pensjon.fagmodul.health.DiagnosticsController"])
-@EnableOAuth2Client
+@EnableOAuth2Client(cacheEnabled = true)
 @SpringBootApplication
 @EnableCaching
 @Profile("!unsecured-webmvctest")
