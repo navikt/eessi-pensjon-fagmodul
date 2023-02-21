6de2a567a213540bed2d52d980e2f7d6d43b9b8d
Daniel Skarpås <dskarpas@gmail.com>
2023-02-21 09:22:42 +0100

    T - Legger inn context test for Token

---
+++ src/test/kotlin/no/nav/eessi/pensjon/integrationtest/EessiTokenValdationTest.kt
@@ -0,0 +1,45 @@
+package no.nav.eessi.pensjon.integrationtest
+
+import com.ninjasquad.springmockk.MockkBean
+import com.ninjasquad.springmockk.MockkBeans
+import no.nav.eessi.pensjon.UnsecuredWebMvcTestLauncher
+import no.nav.eessi.pensjon.fagmodul.config.RestTemplateConfig
+import no.nav.eessi.pensjon.pensjonsinformasjon.clients.PensjonsinformasjonClient
+import no.nav.eessi.pensjon.personoppslag.pdl.PersonService
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.client.RestTemplate
+
+@SpringBootTest(classes = [RestTemplateConfig::class, UnsecuredWebMvcTestLauncher::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = ["unsecured-webmvctest"])
+@EmbeddedKafka
+@EnableMockOAuth2Server
+@AutoConfigureMockMvc
+@MockkBeans(
+    MockkBean(name = "kodeverkRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "prefillOAuthTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "euxSystemRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "safRestOidcRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "pdlRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "euxNavIdentRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "restEuxTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "safGraphQlOidcRestTemplate", classes = [RestTemplate::class]),
+    MockkBean(name = "pensjonsinformasjonClient", classes = [PensjonsinformasjonClient::class]),
+    MockkBean(name = "personService", classes = [PersonService::class])
+)
+class EessiTokenValdationTest {
+
+    @Autowired
+    protected lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `contextTest`(){
+        //alt er vel om vi kommer hit
+    }
+}
