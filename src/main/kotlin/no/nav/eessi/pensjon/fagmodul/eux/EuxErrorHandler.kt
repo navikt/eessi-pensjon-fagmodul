b5555e001d07ec680dcc733c3811aa17baf96b4b
Daniel Skarpås <dskarpas@gmail.com>
2023-02-22 15:03:45 +0100

    r - oppdatert ihht spring 6 med ny tolkning av statuskode

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxErrorHandler.kt
@@ -7,24 +7,23 @@ import java.io.IOException
 
 
 /**
- * Håndtere feil ved rest kall og se til at responsen behandles
+ * kaster en lokal exception basert på http status for server/klient -exception
  */
 open class EuxErrorHandler : ResponseErrorHandler {
     @Throws(IOException::class)
     override fun hasError(response: ClientHttpResponse): Boolean {
-        return response.statusCode.series() == HttpStatus.Series.CLIENT_ERROR
-                || response.statusCode.series() == HttpStatus.Series.SERVER_ERROR
+        return response.statusCode.is4xxClientError || response.statusCode.is5xxServerError
     }
     @Throws(IOException::class)
     override fun handleError(httpResponse: ClientHttpResponse) {
-        if (httpResponse.statusCode.series() === HttpStatus.Series.SERVER_ERROR) {
+        if (httpResponse.statusCode.is5xxServerError) {
             when (httpResponse.statusCode) {
                 HttpStatus.INTERNAL_SERVER_ERROR -> throw EuxRinaServerException("Rina serverfeil, kan også skyldes ugyldig input")
                 HttpStatus.GATEWAY_TIMEOUT -> throw GatewayTimeoutException("Venting på respons fra Rina resulterte i en timeout")
                 else -> throw GenericUnprocessableEntity("En feil har oppstått")
             }
 
-        } else if (httpResponse.statusCode.series() === HttpStatus.Series.CLIENT_ERROR) {
+        } else if (httpResponse.statusCode.is4xxClientError) {
             when (httpResponse.statusCode) {
                 HttpStatus.UNAUTHORIZED -> throw RinaIkkeAutorisertBrukerException("Authorization token required for Rina.")
                 HttpStatus.FORBIDDEN -> throw ForbiddenException("Forbidden, Ikke tilgang")
