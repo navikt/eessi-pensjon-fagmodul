2659051b60296456878fed466599f4090711f8bb
Daniel Skarpås <dskarpas@gmail.com>
2023-03-13 10:44:01 +0100

    r - Sletter ubrukte klasser og metoder som nå er flyttet til ep-eux

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/bucmodel/Creator.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.eessi.pensjon.fagmodul.eux.bucmodel
-
-import no.nav.eessi.pensjon.eux.model.buc.Organisation
-
-//class Creator(
-//        val organisation: Organisation? = null,
-//)
