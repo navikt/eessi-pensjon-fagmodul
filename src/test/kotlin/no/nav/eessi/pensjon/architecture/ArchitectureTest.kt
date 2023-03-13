e98564f9ba09a3297986070f42e3bb84e1cb569e
Daniel Skarpås <dskarpas@gmail.com>
2023-03-13 12:34:12 +0100

    t - Legger tilbake cycle test for archtest

--- src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
@@ -21,7 +21,6 @@ import no.nav.eessi.pensjon.EessiFagmodulApplication
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.context.annotation.Scope
 import org.springframework.web.bind.annotation.GetMapping
@@ -178,7 +177,6 @@ class ArchitectureTest {
                 .check(productionClasses)
     }
 
-    @Disabled
     @Test
     fun `no cycles on any level for production classes`() {
         slices()
