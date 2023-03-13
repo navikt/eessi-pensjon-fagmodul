d473d2ed04fccbd38e25f64dca7b072264796d0a
mariampervez <mariam.pervez@nav.no>
2023-03-13 10:53:04 +0100

    F - Forsøker å legge til Swagger2

--- src/main/kotlin/no/nav/eessi/pensjon/EessiFagmodulApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiFagmodulApplication.kt
@@ -5,14 +5,21 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.retry.annotation.EnableRetry
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation", "no.nav.eessi.pensjon.fagmodul.health.DiagnosticsController"])
 @EnableOAuth2Client(cacheEnabled = true)
 @SpringBootApplication
 @EnableCaching
 @EnableRetry
+@EnableSwagger2
 @Profile("!unsecured-webmvctest")
 class EessiFagmodulApplication
 
@@ -23,4 +30,10 @@ class EessiFagmodulApplication
  */
 fun main(args: Array<String>) {
     runApplication<EessiFagmodulApplication>(*args)
+
+    @Bean
+    fun swagger2(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.basePackage("no.nav.eessi.pensjon.fagmodul")).build()
+    }
 }
