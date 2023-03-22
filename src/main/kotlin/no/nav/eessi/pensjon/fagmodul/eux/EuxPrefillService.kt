6936fea3b024847656466c283a2388aea210ffea
Daniel Skarpås <dskarpas@gmail.com>
2023-03-22 12:21:18 +0100

    f - Hånterer feil ved eux.opprettSed, logger og kaster videre

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxPrefillService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxPrefillService.kt
@@ -66,7 +66,14 @@ class EuxPrefillService (private val euxKlient: EuxKlientLib,
     fun opprettJsonSedOnBuc(jsonNavSED: String, sedType: SedType, euxCaseId: String, vedtakId: String?): BucSedResponse {
         logger.info("Forsøker å opprette en $sedType på rinasakId: $euxCaseId")
 
-        val bucSedResponse = opprettSED.measure { euxKlient.opprettSed(jsonNavSED, euxCaseId) }
+        val bucSedResponse = opprettSED.measure {
+            try {
+                euxKlient.opprettSed(jsonNavSED, euxCaseId)
+            }catch (ex: Exception){
+                logger.error("OpprettSed feiler med melding: ${ex.message}", ex)
+                throw ex
+            }
+        }
 
         statistikk.produserSedOpprettetHendelse(euxCaseId, bucSedResponse.documentId, vedtakId, sedType)
         return bucSedResponse
