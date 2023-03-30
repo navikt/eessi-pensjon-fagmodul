3eaf6d2346e35348c003e2ebcc3d25184348750f
Daniel Skarpås <dskarpas@gmail.com>
2023-03-30 14:46:36 +0200

    R - Legger inn retry for hentRinaSakIderFraMetaData

--- src/main/kotlin/no/nav/eessi/pensjon/vedlegg/VedleggService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/vedlegg/VedleggService.kt
@@ -1,5 +1,6 @@
 package no.nav.eessi.pensjon.vedlegg
 
+import jakarta.annotation.PostConstruct
 import no.nav.eessi.pensjon.eux.model.buc.MissingBuc
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.eessi.pensjon.vedlegg.client.Dokument
@@ -9,9 +10,16 @@ import no.nav.eessi.pensjon.vedlegg.client.HentdokumentInnholdResponse
 import no.nav.eessi.pensjon.vedlegg.client.SafClient
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
+import org.springframework.retry.RetryCallback
+import org.springframework.retry.RetryContext
+import org.springframework.retry.RetryListener
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
-import jakarta.annotation.PostConstruct
+import java.io.IOException
 
 @Service
 class VedleggService(private val safClient: SafClient,
@@ -61,16 +69,33 @@ class VedleggService(private val safClient: SafClient,
     /**
      * Returnerer en distinct liste av rinaSakIDer basert på tilleggsinformasjon i journalposter for en aktør
      */
+    @Retryable(
+        exclude = [IOException::class],
+        backoff = Backoff(delayExpression = "@euxKlientVedleggServiceRetryConfig.initialRetryMillis", maxDelay = 200000L, multiplier = 3.0),
+        listeners  = ["euxKlientVedleggServiceRetryLogger"]
+    )
     fun hentRinaSakIderFraMetaData(aktoerId: String): List<String> =
-        HentRinaSakIderFraDokumentMetadata.measure {
-            hentDokumentMetadata(aktoerId).data.dokumentoversiktBruker.journalposter
-                .flatMap { journalpost ->
-                    journalpost.tilleggsopplysninger
-                        .filter { it["nokkel"].equals(TILLEGGSOPPLYSNING_RINA_SAK_ID_KEY) }
-                        .filter { it["verdi"] != null }
-                        .map { it["verdi"]!! }
-                }.filterNot { MissingBuc.checkForMissingBuc(it) }
-                .distinct()
-                .also { logger.info("Fant følgende RINAID fra dokument Metadata: ${it.map { str -> str }}") }
-        }
+        hentDokumentMetadata(aktoerId).data.dokumentoversiktBruker.journalposter
+            .flatMap { journalpost ->
+                journalpost.tilleggsopplysninger
+                    .filter { it["nokkel"].equals(TILLEGGSOPPLYSNING_RINA_SAK_ID_KEY) }
+                    .filter { it["verdi"] != null }
+                    .map { it["verdi"]!! }
+            }.filterNot { MissingBuc.checkForMissingBuc(it) }
+            .distinct()
+            .also { logger.info("Fant følgende RINAID fra dokument Metadata: ${it.map { str -> str }}") }
+}
+
+
+//TODO: flytte disse til et felles sted, ev en annen løsning
+@Profile("!retryConfigOverride")
+@Component
+data class EuxKlientVedleggServiceRetryConfig(val initialRetryMillis: Long = 20000L)
+
+@Component
+class EuxKlientVedleggServiceRetryLogger : RetryListener {
+    private val logger = LoggerFactory.getLogger(EuxKlientVedleggServiceRetryLogger::class.java)
+    override fun <T : Any?, E : Throwable?> onError(context: RetryContext?, callback: RetryCallback<T, E>?, throwable: Throwable?) {
+        logger.warn("Feil under henting fra EUX - try #${context?.retryCount } - ${throwable?.toString()}", throwable)
+    }
 }
