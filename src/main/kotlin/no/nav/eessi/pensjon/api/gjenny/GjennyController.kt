f9a5f7d77dffe2d6aeaf1106a3646b70917a4eb7
mariampervez <mariam.pervez@nav.no>
2023-11-15 12:42:11 +0100

    f - Legger til /gjenny/bucs i gjennyController
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/api/gjenny/GjennyController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/gjenny/GjennyController.kt
@@ -1,17 +1,18 @@
 package no.nav.eessi.pensjon.api.gjenny
 
 import no.nav.eessi.pensjon.fagmodul.eux.EuxInnhentingService
+import no.nav.eessi.pensjon.fagmodul.eux.ValidBucAndSed
 import no.nav.eessi.pensjon.fagmodul.prefill.InnhentingService
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import kotlin.time.ExperimentalTime
 
 @Unprotected
 @RestController
@@ -35,6 +36,9 @@ class GjennyController (
         bucerForAvdodGjenny = metricsHelper.init("bucerForAvdodGjenny", ignoreHttpCodes = listOf(HttpStatus.FORBIDDEN))
         bucViewGjenny = metricsHelper.init("BucView", ignoreHttpCodes = listOf(HttpStatus.FORBIDDEN))
     }
+
+    @GetMapping("/bucs", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getBucs() = ValidBucAndSed.pensjonsBucer()
 
     @GetMapping("/rinasaker/{aktoerId}/avdodfnr/{avdodfnr}")
     fun getGjenlevendeRinasakerAvdodGjenny(
