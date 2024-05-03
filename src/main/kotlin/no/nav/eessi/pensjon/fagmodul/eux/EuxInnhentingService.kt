8d760d6cc8b07b7503ef5e4d512c2b7cf1838cc3
Daniel Skarpås <dskarpas@gmail.com>
2024-05-03 11:18:54 +0200

    B - Legger inn sjekker for null verdier for rinasaker

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxInnhentingService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/eux/EuxInnhentingService.kt
@@ -301,15 +301,25 @@ class EuxInnhentingService (@Value("\${ENV}") private val environment: String,
 
         return HentRinasaker.measure {  euxKlient.getRinasaker(fnr = fnr, euxCaseId = null)
             .filter { erRelevantForVisningIEessiPensjon(it) }
-            .map { rinasak ->
-                BucView(
-                    rinasak.id!!,
-                    BucType.from(rinasak.processDefinitionId)!!,
-                    aktoerId,
-                    pesysSaksnr,
-                    null,
-                    BucViewKilde.BRUKER
-                )
+            .mapNotNull { rinasak ->
+                if (rinasak.id == null) {
+                    logger.warn("rinasak.id er null")
+                }
+                if (rinasak.processDefinitionId == null) {
+                    logger.warn("rinasak.processDefinitionId er null")
+                }
+                if (rinasak.id != null && rinasak.processDefinitionId != null) {
+                    BucView(
+                        rinasak.id!!,
+                        BucType.from(rinasak.processDefinitionId),
+                        aktoerId,
+                        pesysSaksnr,
+                        null,
+                        BucViewKilde.BRUKER
+                    )
+                } else {
+                    null
+                }
             }.also {
                 val end = System.currentTimeMillis()
                 logger.info("hentBucViewBruker tid ${end - start} i ms")
