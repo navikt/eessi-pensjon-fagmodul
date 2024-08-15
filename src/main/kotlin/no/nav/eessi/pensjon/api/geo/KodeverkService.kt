250eb6752628029e97f6bc1c682a954c0b3c269f
Daniel Skarpås <dskarpas@gmail.com>
2024-08-15 12:27:35 +0200

    F - Legger til REST api for henting av landkoder fra TEN

---
+++ src/main/kotlin/no/nav/eessi/pensjon/api/geo/KodeverkService.kt
@@ -0,0 +1,24 @@
+package no.nav.eessi.pensjon.api.geo
+
+import org.springframework.http.*
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+
+@Service
+class KodeverkService(private val euxNavIdentRestTemplate: RestTemplate) {
+
+    fun getLandkoderAkseptertAvRina(format: String? = null): String? {
+        val url = "/cpi/landkoder/rina"
+
+        val response: ResponseEntity<String> = euxNavIdentRestTemplate.exchange(
+            if (format != null) "$url?format=$format" else url,
+            HttpMethod.GET,
+            HttpEntity<String>(HttpHeaders().apply {
+                contentType = MediaType.APPLICATION_JSON
+            }),
+            String::class.java
+        )
+
+        return response.body
+    }
+}
