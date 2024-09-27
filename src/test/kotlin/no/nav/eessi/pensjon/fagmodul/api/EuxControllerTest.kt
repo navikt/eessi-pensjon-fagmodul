3830fd63d3393e40abf685290d2fe439aa1ef68c
Daniel Skarpås <dskarpas@gmail.com>
2024-09-27 15:47:46 +0200

    T - Lager egne versjoner for restTemplate for å bedre kunne styre hva som sendes ut på det store nettet

--- src/test/kotlin/no/nav/eessi/pensjon/fagmodul/api/EuxControllerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/fagmodul/api/EuxControllerTest.kt
@@ -5,17 +5,19 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.SpyK
 import io.mockk.mockk
-import io.mockk.verify
 import no.nav.eessi.pensjon.eux.klient.EuxKlientAsSystemUser
 import no.nav.eessi.pensjon.eux.model.BucType.P_BUC_06
 import no.nav.eessi.pensjon.fagmodul.eux.EuxInnhentingService
-import no.nav.eessi.pensjon.fagmodul.eux.EuxKlientTest
 import no.nav.eessi.pensjon.shared.api.InstitusjonItem
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
 class EuxControllerTest {
@@ -26,10 +28,13 @@ class EuxControllerTest {
     private lateinit var euxController: EuxController
 
     private val backupList = listOf("AT", "BE", "BG", "CH", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "HR", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MT", "NL", "NO", "PL", "PT", "RO", "SE", "SI", "SK", "UK")
+    private val euxRestTemplate = mockk<RestTemplate>()
+    private val euxSystemRestTemplate = mockk<RestTemplate>()
 
     @BeforeEach
     fun before() {
-        mockEuxInnhentingService = EuxInnhentingService("Q2", EuxKlientAsSystemUser(RestTemplate(), RestTemplate()))
+
+        mockEuxInnhentingService = EuxInnhentingService("Q2", EuxKlientAsSystemUser(euxRestTemplate, euxSystemRestTemplate))
 
         MockKAnnotations.init(this, relaxed = true, relaxUnitFun = true)
 
@@ -62,12 +67,16 @@ class EuxControllerTest {
 
     @Test
     fun `Gitt at vi skal sende en P2000 saa returneres true etter sending`() {
-        every { mockEuxInnhentingService.sendSed(any(), any()) } returns true
+        val euxCaseId = "111"
+        val dokumentId = "222"
+        val path = "/buc/{$euxCaseId}/sed/{$dokumentId}/send?ventePaAksjon=false"
+        every {
+            euxRestTemplate.exchange(path, HttpMethod.POST, any<HttpEntity<String>>(), String::class.java)
+        } returns ResponseEntity("", HttpStatus.OK)
 
-        val result = euxController.sendSeden("12345", "P2000dokid")
+        val result = euxController.sendSeden(euxCaseId, dokumentId)
 
         assertEquals(true, result.statusCode.is2xxSuccessful)
-
     }
 }
 
