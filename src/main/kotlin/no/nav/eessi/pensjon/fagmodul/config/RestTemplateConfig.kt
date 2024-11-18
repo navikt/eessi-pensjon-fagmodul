078ee7b3f80dedfcb44ec3e0cc83386dce6e7b0c
Daniel Skarpås <dskarpas@gmail.com>
2024-11-18 13:02:58 +0100

    F! Oppdaterer access token til access_token

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/config/RestTemplateConfig.kt
@@ -132,7 +132,7 @@ class RestTemplateConfig(
     ): ClientHttpRequestInterceptor {
         return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
             val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
-            response.accessToken?.let { request.headers.setBearerAuth(it) }
+            response.access_token?.let { request.headers.setBearerAuth(it) }
             execution.execute(request, body!!)
         }
     }
