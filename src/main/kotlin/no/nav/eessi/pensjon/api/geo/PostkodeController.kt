1984bf747d43879442c2f24e022312adaee498b4
mariampervez <mariam.pervez@nav.no>
2025-04-22 14:25:47 +0200

    F - Bumper opp kodeverk fra 4.1.36 -> 4.1.46

--- src/main/kotlin/no/nav/eessi/pensjon/api/geo/PostkodeController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/geo/PostkodeController.kt
@@ -1,25 +1,24 @@
 package no.nav.eessi.pensjon.api.geo
 
-import no.nav.eessi.pensjon.kodeverk.PostnummerService
-import no.nav.security.token.support.core.api.Unprotected
-import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@Unprotected
-@RestController
-@RequestMapping("/postnummer")
-class PostkodeController(private val postnummerService: PostnummerService) {
-
-    private val logger = LoggerFactory.getLogger(PostkodeController::class.java)
-
-    @GetMapping("/{postnr}/sted")
-    fun getPostSted(@PathVariable("postnr", required = true) postnr: String): String? {
-
-        logger.info("Henter poststed fra postnr: $postnr")
-        return postnummerService.finnPoststed(postnr)
-    }
-
-}
+//import no.nav.security.token.support.core.api.Unprotected
+//import org.slf4j.LoggerFactory
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.PathVariable
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//@Unprotected
+//@RestController
+//@RequestMapping("/postnummer")
+//class PostkodeController() {
+//
+//    private val logger = LoggerFactory.getLogger(PostkodeController::class.java)
+//
+//    @GetMapping("/{postnr}/sted")
+//    fun getPostSted(@PathVariable("postnr", required = true) postnr: String): String? {
+//
+//        logger.info("Henter poststed fra postnr: $postnr")
+//        return postnummerService.finnPoststed(postnr)
+//    }
+//
+//}
