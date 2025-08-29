bba266086126016c3ace74bd355fc86bf68a141d
Daniel Skarpås <dskarpas@gmail.com>
2025-08-29 09:28:34 +0200

    F - Tar bort ubrukte metoder for landkode

--- src/main/kotlin/no/nav/eessi/pensjon/api/geo/LandkodeController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/geo/LandkodeController.kt
@@ -16,57 +16,57 @@ class LandkodeController(private val kodeverkClient: KodeverkClient, private val
 
     private val logger = LoggerFactory.getLogger(LandkodeController::class.java)
 
-    @GetMapping("/")
-    fun getLandKoder(): ResponseEntity<FrontEndResponse<String>> {
-        logger.info("Henter landkoder, default")
-        return try {
-            val hentLandkoder = kodeverkClient.hentAlleLandkoder()
-            ResponseEntity.ok(FrontEndResponse(result = hentLandkoder, status = HttpStatus.OK.value().toString()))
-        } catch (ex: Exception) {
-            logger.error("Feil ved henting av landkoder: ${ex.message}", ex)
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
-        }
-    }
-
-    @GetMapping("/landkoder2")
-    fun getLandKode2(): ResponseEntity<FrontEndResponse<List<String>>> {
-        logger.info("Henter landkoder i ISO Alpha2 standard")
-        return try {
-            val listeLandkoder2 = kodeverkClient.hentLandkoderAlpha2()
-            ResponseEntity.ok(FrontEndResponse(result = listeLandkoder2, status = HttpStatus.OK.value().toString()))
-        } catch (ex: Exception) {
-            logger.error("Feil ved henting av landkoder2: ${ex.message}", ex)
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
-        }
-    }
-
-    @GetMapping("/{land2}/land3")
-    fun getLandKoderAlpha3(@PathVariable("land2", required = true) land2: String): ResponseEntity<FrontEndResponse<String>>? {
-        logger.info("Henter landkoder i ISO ALpha 3 standard")
-        return try {
-            val landkoder3 = kodeverkClient.finnLandkode(land2)
-            ResponseEntity.ok(FrontEndResponse(result = landkoder3, status = HttpStatus.OK.value().toString()))
-        } catch (ex: Exception) {
-            logger.error("Feil ved henting av landkoder3: ${ex.message}", ex)
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
-        }
-    }
-
-    @GetMapping("/{land3}/land2")
-    fun getLandKoderAlpha2(@PathVariable("land3", required = true) land3: String): ResponseEntity<FrontEndResponse<String>>? {
-        logger.info("Henter Alpha2 landkode for Alpha3")
-        return try {
-            val landkode2 = kodeverkClient.finnLandkode(land3)
-            ResponseEntity.ok(FrontEndResponse(result = landkode2, status = HttpStatus.OK.value().toString()))
-        } catch (ex: Exception) {
-            logger.error("Feil ved henting av landkoder2: ${ex.message}", ex)
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
-        }
-    }
+//    @GetMapping("/")
+//    fun getLandKoder(): ResponseEntity<FrontEndResponse<String>> {
+//        logger.info("Henter landkoder, default")
+//        return try {
+//            val hentLandkoder = kodeverkClient.hentAlleLandkoder()
+//            ResponseEntity.ok(FrontEndResponse(result = hentLandkoder, status = HttpStatus.OK.value().toString()))
+//        } catch (ex: Exception) {
+//            logger.error("Feil ved henting av landkoder: ${ex.message}", ex)
+//            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
+//        }
+//    }
+//
+//    @GetMapping("/landkoder2")
+//    fun getLandKode2(): ResponseEntity<FrontEndResponse<List<String>>> {
+//        logger.info("Henter landkoder i ISO Alpha2 standard")
+//        return try {
+//            val listeLandkoder2 = kodeverkClient.hentLandkoderAlpha2()
+//            ResponseEntity.ok(FrontEndResponse(result = listeLandkoder2, status = HttpStatus.OK.value().toString()))
+//        } catch (ex: Exception) {
+//            logger.error("Feil ved henting av landkoder2: ${ex.message}", ex)
+//            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
+//        }
+//    }
+//
+//    @GetMapping("/{land2}/land3")
+//    fun getLandKoderAlpha3(@PathVariable("land2", required = true) land2: String): ResponseEntity<FrontEndResponse<String>>? {
+//        logger.info("Henter landkoder i ISO ALpha 3 standard")
+//        return try {
+//            val landkoder3 = kodeverkClient.finnLandkode(land2)
+//            ResponseEntity.ok(FrontEndResponse(result = landkoder3, status = HttpStatus.OK.value().toString()))
+//        } catch (ex: Exception) {
+//            logger.error("Feil ved henting av landkoder3: ${ex.message}", ex)
+//            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
+//        }
+//    }
+//
+//    @GetMapping("/{land3}/land2")
+//    fun getLandKoderAlpha2(@PathVariable("land3", required = true) land3: String): ResponseEntity<FrontEndResponse<String>>? {
+//        logger.info("Henter Alpha2 landkode for Alpha3")
+//        return try {
+//            val landkode2 = kodeverkClient.finnLandkode(land3)
+//            ResponseEntity.ok(FrontEndResponse(result = landkode2, status = HttpStatus.OK.value().toString()))
+//        } catch (ex: Exception) {
+//            logger.error("Feil ved henting av landkoder2: ${ex.message}", ex)
+//            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body(FrontEndResponse(status = HttpStatus.INTERNAL_SERVER_ERROR.name, message = ex.message))
+//        }
+//    }
 
     @GetMapping("/rina")
     fun landkoderAkseptertAvRina(@RequestParam(required = false) format: String?): ResponseEntity<FrontEndResponse<LandkodeMerKorrektFormat>>? {
