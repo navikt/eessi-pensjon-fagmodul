232c0c0563e18fcee31a58d2cddaa9f33d53bf92
Daniel Skarpås <dskarpas@gmail.com>
2025-09-01 13:50:32 +0200

    Revert "R - Prøver getSedOnBucByDocumentId"
    This reverts commit a3ef9567797e7ff684be55a83b31e4d1add98617.

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/pesys/PensjonsinformasjonUtlandController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/pesys/PensjonsinformasjonUtlandController.kt
@@ -101,7 +101,7 @@ class PensjonsinformasjonUtlandController(
             logger.info("P6000Detaljer: ${p6000Detaljer.toJson()}")
             runCatching {
                 p6000Detaljer.dokumentId.forEach { p6000 ->
-                    val hentetJsonP6000 = euxInnhentingService.getSedOnBucByDocumentId(p6000Detaljer.rinaSakId, p6000)
+                    val hentetJsonP6000 = euxInnhentingService.getSedOnBucByDocumentIdAsSystemuser(p6000Detaljer.rinaSakId, p6000)
                             val somP6000 = hentetJsonP6000 as P6000
                     logger.info("somP6000: $somP6000")
                     somP6000.let { listeOverP6000FraGcp.add(it) }
