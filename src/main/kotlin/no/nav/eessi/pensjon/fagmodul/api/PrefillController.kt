a567f89641e2f4e921869fd68cf0812e379341e3
Daniel Skarpås <dskarpas@gmail.com>
2025-09-09 09:55:43 +0200

    F - Legger inn sperre for lagring av P7000

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/PrefillController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/PrefillController.kt
@@ -168,8 +168,8 @@ class PrefillController(
 
         //Lagrer P6000 detaljer til GCP Storage
         try {
-            if(request.sed == SedType.P6000) {
-                logger.debug("Lagerer p6000: ${request.payload}")
+            if(request.sed == SedType.P7000) {
+                logger.debug("Lagerer P7000: ${request.payload}")
                 request.payload?.let { mapJsonToAny<List<P6000Dokument>>(it) }?.let { listeOverP6000 ->
                     gcpStorageService.lagretilBackend(
                         PensjonsinformasjonUtlandController.P6000Detaljer(
