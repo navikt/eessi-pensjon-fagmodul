a58decbd742bcbb27b8c0113eafd17da9236c7bd
Daniel Skarpås <dskarpas@gmail.com>
2025-10-14 09:55:08 +0200

    F - Utvider FrontEndResponse med  SuccessResponse og ErrorResponse

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/FrontendReponse.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/FrontendReponse.kt
@@ -1,8 +1,11 @@
 package no.nav.eessi.pensjon.fagmodul.api
 
-data class FrontEndResponse<T>(
+class FrontEndResponse<T>(
     val result: T? = null,
     val status: String? = null,
     val message: String? = null,
     val stackTrace: String? = null
 )
+
+//class SuccessResponse<T>(result: T) : FrontEndResponse<T>(result = result, status = "OK")
+//class ErrorResponse<T>(message: String, stackTrace: String? = null) : FrontEndResponse<T>(status = "ERROR", message = message, stackTrace = stackTrace)
