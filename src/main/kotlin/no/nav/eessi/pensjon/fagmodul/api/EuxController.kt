ed23be7b44db9f0c19118a7a802d2cd41eac1836
Daniel Skarpås <dskarpas@gmail.com>
2026-04-09 13:05:38 +0200

    F - Merge euxController fra master

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/EuxController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/EuxController.kt
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.HttpStatusCodeException
-import org.springframework.web.server.ResponseStatusException
 
 
 @RestController
@@ -25,14 +24,14 @@ class EuxController(
 ) {
     private val logger = LoggerFactory.getLogger(EuxController::class.java)
 
-    private lateinit var rinaUrl: MetricsHelper.Metric
-    private lateinit var resend: MetricsHelper.Metric
-    private lateinit var resendMedRinaId: MetricsHelper.Metric
-    private lateinit var sedsendt: MetricsHelper.Metric
-    private lateinit var euxKodeverk: MetricsHelper.Metric
-    private lateinit var paakobledeland: MetricsHelper.Metric
-    private lateinit var euxKodeverkLand: MetricsHelper.Metric
-    private lateinit var euxInstitusjoner: MetricsHelper.Metric
+    private  var rinaUrl: MetricsHelper.Metric
+    private  var resend: MetricsHelper.Metric
+    private  var resendMedRinaId: MetricsHelper.Metric
+    private  var sedsendt: MetricsHelper.Metric
+    private  var euxKodeverk: MetricsHelper.Metric
+    private  var paakobledeland: MetricsHelper.Metric
+    private  var euxKodeverkLand: MetricsHelper.Metric
+    private  var euxInstitusjoner: MetricsHelper.Metric
 
     init {
             rinaUrl = metricsHelper.init("RinaUrl")
@@ -49,13 +48,13 @@ class EuxController(
 
     @Unprotected
     @GetMapping("/rinaurl")
-    fun getRinaUrl2020(): ResponseEntity<Map<String, String>> = rinaUrl.measure {
-        return@measure ResponseEntity.ok(mapOf("rinaUrl" to euxInnhentingService.getRinaUrl()))
+    fun getRinaUrl2020(): ResponseEntity<FrontEndResponse<Map<String, String>>> = rinaUrl.measure {
+        return@measure ResponseEntity.ok(FrontEndResponse(mapOf("rinaUrl" to euxInnhentingService.getRinaUrl())))
     }
 
     @Protected
     @GetMapping("/countries/{buctype}")
-    fun getPaakobledeland(@PathVariable(value = "buctype") bucType: BucType): ResponseEntity<FrontEndResponse<String>> {
+    fun getPaakobledeland(@PathVariable(value = "buctype") bucType: BucType): ResponseEntity<FrontEndResponse<List<String>>> {
         return paakobledeland.measure {
             logger.info("Henter ut liste over land knyttet til buc: $bucType")
             return@measure try {
@@ -66,11 +65,11 @@ class EuxController(
                     logger.warn("Ingen svar fra /institusjoner?BuCType, kjører backupliste")
                     backupList
                 }
-                ResponseEntity.ok(FrontEndResponse(landlist.toJson(), HttpStatus.OK.name))
+                ResponseEntity.ok(FrontEndResponse(landlist, HttpStatus.OK.name))
             } catch (sce: HttpStatusCodeException) {
-                ResponseEntity.status(sce.statusCode).body(FrontEndResponse(errorBody(sce.responseBodyAsString), sce.statusCode.toString()))
+                ResponseEntity.status(sce.statusCode).body(FrontEndResponse(null, errorBody(sce.responseBodyAsString), sce.statusCode.toString()))
             } catch (ex: Exception) {
-                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(FrontEndResponse(ex.message?.let { errorBody(it) }, HttpStatus.INTERNAL_SERVER_ERROR.toString()))
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(FrontEndResponse(null, ex.message?.let { errorBody(it) }, HttpStatus.INTERNAL_SERVER_ERROR.toString()))
             }
         }
     }
@@ -101,11 +100,12 @@ class EuxController(
                     logger.info("Sed er sendt til Rina")
                     ResponseEntity.ok().body(FrontEndResponse("Sed er sendt til Rina", HttpStatus.OK.name))
                 } else {
-                    throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Sed ble ikke sendt til Rina")
+                    logger.error("Sed ble ikke sendt til Rina")
+                    ResponseEntity.badRequest().body(FrontEndResponse("Sed ble IKKE sendt til Rina", HttpStatus.BAD_REQUEST.name))
                 }
             } catch (ex: Exception) {
-                logger.error("En uventet feil har oppstått, sed ble IKKE sendt til Rina ${ex.message}")
-                ResponseEntity.unprocessableEntity().body(FrontEndResponse("Sed ble IKKE sendt til Rina", HttpStatus.BAD_REQUEST.name, ex.message))
+                logger.error("Sed ble ikke sendt til Rina")
+                ResponseEntity.badRequest().body(FrontEndResponse("Sed ble IKKE sendt til Rina", HttpStatus.BAD_REQUEST.name))
             }
         }
     }
