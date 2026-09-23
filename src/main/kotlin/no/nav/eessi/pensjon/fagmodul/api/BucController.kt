93e9ce10f18816f534bd3a3266107b588b64e7ff
Daniel Skarpås <dskarpas@gmail.com>
2026-09-23 05:04:43 -0700

    Feature/fjerner aktorid fra meta (#568)
    * F - Fjerner aktørID fra GET metadata (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/BucController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/BucController.kt
@@ -66,8 +66,25 @@ class BucController(
             FrontEndResponse(ValidBucAndSed.pensjonsBucer(), HttpStatus.OK.name)
         }
 
-    @GetMapping("/{euxcaseid}/metadata/{aktoerId}")
+    @GetMapping("/{euxcaseid}/metadata")
     fun hentBucMedMetadata(
+        @PathVariable(value = "euxcaseid", required = true) euxcaseid: String
+    ): FrontEndResponse<BucAndSedView> =
+        timedControllerCall("hentBucMedMetadata") {
+            bucDetaljerEnkel.measure {
+                auditlogger.logBuc("hentSingleBucAndSedView", "euxcaseId:$euxcaseid")
+                logger.debug(" prøver å hente ut en enkel buc med euxCaseId: $euxcaseid")
+                val enkeltBucAndSedView = euxInnhentingService.getSingleBucAndSedViewMedMetadata(euxcaseid)
+                if (enkeltBucAndSedView.error.isNullOrEmpty()) {
+                    FrontEndResponse(enkeltBucAndSedView, HttpStatus.OK.name)
+                } else {
+                    FrontEndResponse(enkeltBucAndSedView, enkeltBucAndSedView.error)
+                }
+            }
+        }
+
+    @GetMapping("/{euxcaseid}/metadata/{aktoerId}")
+    fun hentBucMedMetadataTo(
         @PathVariable(value = "euxcaseid", required = true) euxcaseid: String,
         @PathVariable(value = "aktoerId", required = false) aktoerId: String,
     ): FrontEndResponse<BucAndSedView> =
